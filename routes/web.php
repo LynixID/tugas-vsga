c67492e24994528aedb9167b3974f5a33473c571
Riancahyo <riancahyo75@gmail.com>
2024-07-16 09:51:37 +0700

    kkk

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('beranda');
 });
+
+// Rute untuk menampilkan halaman login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// Rute untuk menangani permintaan login
+Route::post('/login', [LoginController::class, 'login']);
