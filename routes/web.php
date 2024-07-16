1ffc95828d1f6aeab9e27693b07d69d5e1b43992
LAPTOP-3K3QP9QJ\Maharani <maharanidwi883@gmail.com>
2024-07-16 10:08:23 +0700

    membenahi url beranda

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::resource('admin/books', BookController::class)->middleware('auth');
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 });
 Route::get('/login', function () {
     return view('login');
